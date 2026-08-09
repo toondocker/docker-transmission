dd04e070542c9ae83ecd857ed9c22f28465ba2b0
Nigel <46616363+toondocker@users.noreply.github.com>
2026-08-09 14:20:05 -0400

    Update blocklist-update.sh
    fix shellcheck issues

--- root/app/blocklist-update.sh
+++ root/app/blocklist-update.sh
@@ -6,8 +6,8 @@ PORT=$(jq '.["rpc-port"]' /config/settings.json)
 
 if [[ "$BLOCKLIST_ENABLED" == true ]]; then
     if [[ -n "$USER" ]] && [[ -n "$PASS" ]]; then
-        /usr/bin/transmission-remote 127.0.0.1:${PORT:-9091} -n "$USER":"$PASS" --blocklist-update
+        /usr/bin/transmission-remote "127.0.0.1:${PORT:-9091}" -n "$USER":"$PASS" --blocklist-update
     else
-        /usr/bin/transmission-remote 127.0.0.1:${PORT:-9091} --blocklist-update
+        /usr/bin/transmission-remote "127.0.0.1:${PORT:-9091}" --blocklist-update
     fi
 fi
