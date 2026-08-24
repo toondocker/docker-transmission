06ccb8933e782b23614716e8c3d35d099d09d619
Nigel <46616363+toondocker@users.noreply.github.com>
2026-08-23 22:12:51 -0400

    Update symlink-videos.sh
    fix shellcheck issues

--- root/app/symlink-videos.sh
+++ root/app/symlink-videos.sh
@@ -389,16 +389,13 @@ $(_sonarr_extract_series "$_var_clean")
 EOF
     log_debug "Sonarr response: title=[$_title] tvdbId=[$_tvdbId] year=[$_year] tmdbId=[$_tmdbId] cleanTitle=[$_cleanTitle] - via=[$_var_clean]"
     SERIES_TITLE="$_title"
-    # shellcheck disable=SC2236
-    if [ ! -n "$_year" ]; then
+    if [ -n "$_year" ]; then
         SERIES_TITLE="$SERIES_TITLE ($_year)"
     fi
-    # shellcheck disable=SC2236
-    if [ ! -z "$_tvdbId" ]; then
+    if [ -n "$_tvdbId" ]; then
         SERIES_TITLE="$SERIES_TITLE [tvdbid-$_tvdbId]"
     fi
-    # shellcheck disable=SC2236
-    if [ ! -z "$_tmdbId" ]; then
+    if [ -n "$_tmdbId" ]; then
         SERIES_TITLE="$SERIES_TITLE [tmdbid-$_tmdbId]"
     fi
     log_info "Sonarr match: id=[$SERIES_ID] title=[$SERIES_TITLE] via=[$1]"
